db65979e12f188b2f0453e1b95aa60e148b7717e
Arthur Garza <arthur.garzajr@gmail.com>
2017-06-08 18:00:22 -0500

    Initial Commit

---
+++ VGG16/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VGG16
+//
+//  Created by Arthur Garza on 6/8/17.
+//  Copyright © 2017 Arthur Garza. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
